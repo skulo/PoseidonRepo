d7b032e4d4347974f6a1a78c1def9730f0f41744
Varga Boldizsár <boldizsar.varga@coincash.eu>
2025-02-01 12:41:12 +0100

    asdddd

--- main.py
+++ main.py
@@ -36,6 +36,10 @@ from io import BytesIO
 import uuid
 
 
+AWS_ACCESS_KEY_ID = os.getenv('AWS_ACCESS_KEY_ID')
+AWS_SECRET_ACCESS_KEY = os.getenv('AWS_SECRET_ACCESS_KEY')
+AWS_REGION_NAME = os.getenv('AWS_REGION_NAME')
+S3_BUCKET_NAME = os.getenv('S3_BUCKET_NAME')
 
 AWS_ACCESS_KEY_ID = os.getenv('AWS_ACCESS_KEY_ID')
 AWS_SECRET_ACCESS_KEY = os.getenv('AWS_SECRET_ACCESS_KEY')
