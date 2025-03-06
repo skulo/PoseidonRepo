5a5a7e3604346f17f975822a3c47c980e7ca86e2
Varga Boldizsár <boldizsar.varga@coincash.eu>
2025-03-06 17:17:23 +0100

    asd

--- main.py
+++ main.py
@@ -50,16 +50,17 @@ import boto3
 from io import BytesIO
 import uuid
 
+#from dotenv import load_dotenv
+
+#load_dotenv()
 
 AWS_ACCESS_KEY_ID = os.getenv('AWS_ACCESS_KEY_ID')
 AWS_SECRET_ACCESS_KEY = os.getenv('AWS_SECRET_ACCESS_KEY')
 AWS_REGION_NAME = os.getenv('AWS_REGION_NAME')
 S3_BUCKET_NAME = os.getenv('S3_BUCKET_NAME')
 
-AWS_ACCESS_KEY_ID = os.getenv('AWS_ACCESS_KEY_ID')
-AWS_SECRET_ACCESS_KEY = os.getenv('AWS_SECRET_ACCESS_KEY')
-AWS_REGION_NAME = os.getenv('AWS_REGION_NAME')
-S3_BUCKET_NAME = os.getenv('S3_BUCKET_NAME')
+
+
 
 class Book(BaseModel):
     name: str
